74628f2bcd7886bc72ecc8a8f715dc3fbcfa53fc
rich <rich.gregorio@nuix.com>
2017-01-03 17:03:17 -0500

    minor tweaks, hard to work at work

--- dlBoxes.py
+++ dlBoxes.py
@@ -1,16 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from time import sleep
 
 baseURL = "https://atariage.com/system_items.php?SystemID=2600&ItemTypeID=BOX"
 
 driver = webdriver.Chrome()
 driver.get(baseURL)
 
-recordsPerPage = Select(driver.find_element_by_name('recordsPerPage'))
-recordsPerPage.select_by_value('All')
+Select(driver.find_element_by_name('recordsPerPage')).select_by_value('All')
 driver.find_element_by_name('btnSubmit').click()
 
-games = driver.find_elements_by_css_selector('table.standard tr')
+games = driver.find_elements_by_css_selector('table.standard tr td:first-of-type a')
 
 for game in games:
     print(game.text)
+    #driver.find_element_by_link_text(game.text).click()
+
+driver.close()
